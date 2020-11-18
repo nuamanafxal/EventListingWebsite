5582031378dd7344a9d8cc97dbde344063400929
nuaman36 <nuaman36@gmail.com>
2020-11-18 13:30:48 -0800

    fixing

--- wp-config.php
+++ wp-config.php
@@ -77,7 +77,8 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true);
+define( 'WP_DEBUG_DISPLAY', false );
 
 /* That's all, stop editing! Happy publishing. */
 
